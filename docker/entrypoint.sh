e6c9322a2c20e11d1a45c747a5f9d6a086b45aad
Ivan Leonidovich <main.line@live.ru>
2025-03-12 20:30:09 +0300

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-# Копирование файла предсказаний, если он не существует
-if [ ! -f "$PREDICTIONS_FILE" ]; then
-    echo "Файл $PREDICTIONS_FILE не найден. Копирую из /app/data/predictions.json"
-    cp /app/data/predictions.json "$PREDICTIONS_FILE"
-    if [ $? -ne 0 ]; then
-        echo "Ошибка при копировании файла предсказаний!"
-        exit 1
-    fi
-fi
-
 # Запуск бота
 echo "Запуск бота..."
 exec python -m app.bot
